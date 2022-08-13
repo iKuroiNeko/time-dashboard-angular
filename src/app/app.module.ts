60bc0cd2d7b47d1f8456024814e5c489f9a64739
unknown <jessica.fraga01@hotmail.com>
2022-08-12 21:58:13 -0300

    Add basic layout and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GridItemComponent } from './components/grid-item/grid-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridItemComponent
   ],
   imports: [
     BrowserModule

02081c82ca27abbc1f3556ea6fd839844eee134d
Jéssica Fraga <jessica.fraga01@hotmail.com>
2022-08-15 22:38:58 -0300

    Add fontawesome and dropdown menu functionality

--- src/app/components/grid-item/grid-item.component.ts
+++ src/app/components/grid-item/grid-item.component.ts
@@ -1,25 +1,42 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-grid-item',
   templateUrl: './grid-item.component.html',
-  styleUrls: ['./grid-item.component.scss']
+  styleUrls: ['./grid-item.component.scss'],
 })
 export class GridItemComponent implements OnInit {
-
   @Input() category: string = '';
 
-  menu: boolean = false
+  @ViewChild('toggleButton') toggleButton: ElementRef;
+  @ViewChild('menu') menu: ElementRef;
 
-  constructor() { }
+  isMenuOpen: boolean = false;
 
-  ngOnInit(): void {
-  
+  constructor(private renderer: Renderer2) {
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (
+        (this.toggleButton &&
+          this.toggleButton.nativeElement.contains(e.target)) ||
+        (this.menu && this.menu.nativeElement.contains(e.target))
+      ) {
+        this.isMenuOpen = true;
+      } else {
+        this.isMenuOpen = false;
+      }
+    });
   }
+
+  ngOnInit(): void {}
 
   openMenu() {
-    this.menu = !this.menu;
-
+    this.isMenuOpen = true;
   }
-
 }
